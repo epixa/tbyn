067c902c2a825fc0dabd2c50cf4de5d647fd6354
Court Ewing <court@epixa.com>
2016-01-10 21:49:03 -0500

    Refined test setup with immutable chai

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const chai = require('chai');
-const rfr = require('rfr');
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
+import Promise from 'bluebird';
+import chai from 'chai';
+import rfr from 'rfr';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import immutableChai from './support/helpers/immutable-chai';
+
+Promise.longStackTraces();
 
 chai.use(sinonChai);
+chai.use(immutableChai);
 
 global.expect = chai.expect;
 global.sinon = sinon;
---
+++ test/support/helpers/immutable-chai.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import { Assertion } from 'chai';
+import { List } from 'immutable';
+
+export default function immutableChair(chai, utils) {
+  Assertion.addProperty('list', assertList);
+
+  Assertion.addChainableMethod('size', assertListSize, function () {
+    utils.flag(this, 'list.size', true);
+  });
+};
+
+function assertList() {
+  this.assert(
+    List.isList(this._obj),
+    'expected #{this} to be a an immutable list',
+    'expected #{this} to not be an immutable List'
+  );
+}
+
+function assertListSize(size) {
+  assertList.call(new Assertion(this._obj));
+
+  new Assertion(this._obj.size).to.be.a('number');
+
+  this.assert(
+    this._obj.size === size,
+    'expected #{this} to have a size of #{exp} but got #{act}',
+    'expected #{this} to not have a size of #{act}',
+    size,
+    this._obj.size
+  );
+}
