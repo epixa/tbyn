a5119d8a0636100b1d044f171f8666de8ddacd8d
Court Ewing <court@epixa.com>
2016-01-13 17:45:35 -0500

    All methods tested in data/accounts

--- test/lib/data/index.spec.js
+++ test/lib/data/index.spec.js
@@ -62,33 +62,66 @@ describe('data/accounts', function () {
       // todo: expect(newDb).to.be.map;
       expect(newDb).not.to.equal(db);
     });
-    it('returned db includes new account', function () {
+    it('also returns the new account object', function () {
       const [ newDb, account ] = accounts.insert(db, accountData);
-      const list = accounts.all(newDb);
-      expect(list).to.include(account);
-    });
-    it('creates an immutable map of the given account data', function () {
-      const [ newDb, account ] = accounts.insert(db, accountData);
-      // todo: expect(account).to.be.map;
+      expect(account).not.to.equal(accountData);
       expect(account.get('id')).to.equal(accountData.id);
       expect(account.get('name')).to.equal(accountData.name);
       expect(account.get('type')).to.equal(accountData.type);
       expect(account.get('on_budget')).to.equal(accountData.on_budget);
       expect(account.get('closed')).to.equal(accountData.closed);
     });
+    it('returned db includes new account', function () {
+      const [ newDb, account ] = accounts.insert(db, accountData);
+      const list = accounts.all(newDb);
+      expect(list).to.include(account);
+    });
   });
 
   describe('#update()', function () {
     it('returns new db', function() {
-      const account = accounts.all(db).first().set('name', 'wat');
+      const account = accounts.all(db).last().set('name', 'wat');
       const [ newDb ] = accounts.update(db, account);
       expect(newDb).not.to.equal(db);
     });
-    it('returned db includes updated account');
-    it('creates an immutable map of the given account data');
+    it('also returns the updated account object', function () {
+      const account = accounts.all(db).last().set('name', 'wat');
+      const [ newDb, newAccount ] = accounts.update(db, account);
+      expect(account).to.equal(newAccount);
+    });
+    it('new db includes updated account', function () {
+      const account = accounts.all(db).last().set('name', 'wat');
+      const [ newDb, newAccount ] = accounts.update(db, account);
+      const newList = accounts.all(newDb);
+      expect(newList).to.include(newAccount);
+    });
+    it('accounts list in new db does not increase in size', function () {
+      const oldList = accounts.all(db);
+      const account = oldList.last().set('name', 'wat');
+      const [ newDb ] = accounts.update(db, account);
+      const newList = accounts.all(newDb);
+      expect(newList).to.have.size(oldList.size);
+    });
   });
 
-  describe('#delete()', function () {
-
+  describe('#remove()', function () {
+    it('returns new db', function () {
+      const account = accounts.all(db).last();
+      const newDb = accounts.remove(db, account);
+      expect(newDb).not.to.equal(db);
+    });
+    it('new db does not include account', function () {
+      const account = accounts.all(db).last();
+      const newDb = accounts.remove(db, account);
+      const list = accounts.all(newDb);
+      expect(newDb).not.to.include(account);
+    });
+    it('accounts list in new db decreases in size by one', function () {
+      const oldList = accounts.all(db);
+      const account = oldList.last();
+      const newDb = accounts.remove(db, account);
+      const newList = accounts.all(newDb);
+      expect(newList).to.have.size(oldList.size - 1);
+    });
   });
 });
