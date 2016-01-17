9b4dc1f6784f04a37741d9d6a2acf7e5d2d71f9b
Court Ewing <court@epixa.com>
2016-01-17 11:28:26 -0500

    Payee schema tests

---
+++ test/lib/data/payees/schema.spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import { defaults } from 'lodash';
+import * as schema from '../../../../lib/data/payees/schema';
+import payeeData, { requiredPayeeData } from '../../../support/fixtures/payee-data';
+import payee from '../../../support/fixtures/payee';
+
+describe('data/payees/schema', function () {
+  describe('#cast()', function () {
+    function callCastWith(overrides) {
+      return schema.cast(defaults(overrides, payeeData));
+    }
+
+    it('returns immutable payee', function () {
+      const payee = schema.cast(payeeData);
+      expect(payee).to.have.keys(schema.defaults);
+    });
+    it('throws if given invalid id', function () {
+      expect(() => callCastWith({ id: 1 })).to.throw(TypeError);
+      expect(() => callCastWith({ id: '' })).to.throw(Error);
+    });
+    it('throws if given invalid name', function () {
+      expect(() => callCastWith({ name: 1 })).to.throw(TypeError);
+      expect(() => callCastWith({ name: '' })).to.throw(Error);
+    });
+  });
+});
