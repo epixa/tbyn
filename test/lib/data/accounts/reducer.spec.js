724663bb5f76d650445787d10eb23a2f3e3ce284
Court Ewing <court@epixa.com>
2016-03-05 11:02:49 -0500

    Accounts redux reducer

---
+++ test/lib/data/accounts/reducer.spec.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import { fromJS } from 'immutable';
+
+import reducer from '../../../../lib/data/accounts/reducer';
+import accountData, { requiredAccountData } from '../../../support/fixtures/account-data';
+
+describe('data/accounts/reducer', function () {
+  context('ADD_ACCOUNT', function () {
+    it('to create new account in list', function () {
+      const stateBefore = undefined;
+      const stateAfter = fromJS([ accountData ]);
+      const state = reducer(stateBefore, {
+        data: requiredAccountData,
+        type: 'ADD_ACCOUNT'
+      });
+      expect(state).to.equal(stateAfter);
+    });
+  });
+
+  context('UPDATE_ACCOUNT', function () {
+    it('to update existing account in list', function () {
+      const account = fromJS(Object.assign({}, accountData, { name: 'nowai' }));
+      const stateBefore = fromJS([ accountData ]);
+      const stateAfter = fromJS([ account ]);
+      const state = reducer(stateBefore, {
+        account,
+        type: 'UPDATE_ACCOUNT'
+      });
+      expect(state).to.equal(stateAfter);
+    });
+  });
+
+  context('DELETE_ACCOUNT', function () {
+    it('to remove existing account from list', function () {
+      const account = fromJS(accountData);
+      const otherAccount = fromJS(Object.assign({}, accountData, { id: '234' }));
+      const stateBefore = fromJS([ account, otherAccount ]);
+      const stateAfter = fromJS([ otherAccount ]);
+      const state = reducer(stateBefore, {
+        account,
+        type: 'DELETE_ACCOUNT'
+      });
+      expect(state).to.equal(stateAfter);
+    });
+  });
+});
