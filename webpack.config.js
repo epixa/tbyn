6d001c3adceae516043840088092fa8f6f09e5a5
Court Ewing <court@epixa.com>
2016-03-20 12:29:09 -0400

    Dev server with hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader']
+        loaders: ['react-hot', 'babel-loader']
       },
       {
         test: /\.html$/,
