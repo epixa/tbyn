e2144210742039693c65ed217dcb410e147956d4
Court Ewing <court@epixa.com>
2016-04-10 09:16:37 -0400

    AddAccount form validation with account type

--- src/components/add-account/form.jsx
+++ src/components/add-account/form.jsx
@@ -1,17 +1,44 @@
 'use strict';
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const AddAccountForm = ({ fields, handleSubmit }) => (
+const AddAccountForm = ({
+  fields: { name, on_budget },
+  handleSubmit,
+  submitting,
+  submitFailed
+}) => (
   <form onSubmit={handleSubmit}>
+    {submitFailed && <div>There was an error with your form submission</div>}
+
     <label>
       Name
-      <input type="text" {...fields.name} />
+      <input type="text" {...name}/>
     </label>
-    <button type="submit">
-      Submit
+    {name.touched && name.error && <div>{name.error}</div>}
+
+    <label>
+      <input type="radio" name="on_budget" {...on_budget} value="1" checked={on_budget.value === '1'}/>
+      Budget account
+      <span> - This account should affect my budget</span>
+    </label>
+    <label>
+      <input type="radio" name="on_budget" {...on_budget} value="0" checked={on_budget.value === '0'}/>
+      Off-Budget
+      <span> - This account should not affect my budget</span>
+    </label>
+    {on_budget.touched && on_budget.error && <div>{on_budget.error}</div>}
+
+    <button type="submit" disabled={submitting}>
+      {submitting ? <span>Submitting...</span> : <span>Submit</span>}
     </button>
   </form>
 );
+
+AddAccountForm.propTypes = {
+  fields: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  submitting: PropTypes.bool.isRequired
+};
 
 export default AddAccountForm;
--- src/containers/add-account/form.js
+++ src/containers/add-account/form.js
@@ -1,19 +1,33 @@
 'use strict';
 
-import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 
 import AddAccountForm from '../../components/add-account/form';
 import { createAccount, hideAddAccount } from '../../actions/accounts';
 
-const mapDispatchProps = (dispatch, props) => ({
-  handleSubmit(e) {
-    e.preventDefault();
-    dispatch(createAccount({ name: props.fields.name.value }));
+const validate = ({ name, on_budget }) => {
+  const errors = {};
+
+  if (!name) errors.name = 'Name is required';
+
+  if (!on_budget) errors.on_budget = 'Budget status is required';
+
+  return errors;
+};
+
+const strNumToBool = val => {
+  return Boolean(Number(val));
+}
+
+const mapDispatchProps = dispatch => ({
+  onSubmit({ name, on_budget }) {
+    on_budget = strNumToBool(on_budget);
+    dispatch(createAccount({ name, on_budget }));
   }
 });
 
 export default reduxForm({
   form: 'add-account',
-  fields: [ 'name' ]
-})(connect(null, mapDispatchProps)(AddAccountForm));
+  fields: [ 'name', 'on_budget' ],
+  validate
+}, null, mapDispatchProps)(AddAccountForm);
