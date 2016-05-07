2dc1547ea8d736b1ba442833b4ba35be1311d12e
Court Ewing <court@epixa.com>
2016-05-07 12:38:59 -0400

    Support for enzyme in tests

--- test/setup.js
+++ test/setup.js
@@ -5,11 +5,13 @@ import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import chaiImmutable from 'chai-immutable';
+import chaiEnzyme from 'chai-enzyme';
 
 Promise.longStackTraces();
 
 chai.use(sinonChai);
 chai.use(chaiImmutable);
+chai.use(chaiEnzyme);
 
 global.expect = chai.expect;
 global.sinon = sinon;
