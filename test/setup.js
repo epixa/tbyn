b8c4d465f282291e8a588c1182ea8dc47024d1f2
Court Ewing <court@epixa.com>
2016-05-21 12:03:43 -0400

    Support for global window and DOM apis in tests
    Enzyme's mount() functionality will depend on these standard browser
    APIs being available globally even though the tests are run through
    node.

--- test/setup.js
+++ test/setup.js
@@ -6,6 +6,8 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import chaiImmutable from 'chai-immutable';
 import chaiEnzyme from 'chai-enzyme';
+import { jsdom } from 'jsdom';
+import { pickBy } from 'lodash';
 
 Promise.longStackTraces();
 
@@ -15,3 +17,11 @@ chai.use(chaiEnzyme);
 
 global.expect = chai.expect;
 global.sinon = sinon;
+
+global.document = jsdom('');
+global.window = document.defaultView;
+global.navigator = { userAgent: 'node.js' };
+Object.assign(
+  global,
+  pickBy(global.window, (val, key) => typeof global[key] === 'undefined')
+);
